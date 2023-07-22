46f493016341df620bcd6cfa35450689fbfb3d73
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-22 12:00:48 +0300

    Initial commit. Product Service.

---
+++ src/test/java/sprojects/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sprojects.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
