33b37e7e001a74b3db1c23c7d3d2c05704ed22bd
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-22 13:50:51 +0300

    Service. ProductService.

--- src/main/java/sprojects/productservice/controller/ProductController.java
+++ src/main/java/sprojects/productservice/controller/ProductController.java
@@ -7,15 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import sprojects.productservice.dto.ProductRequest;
+import sprojects.productservice.service.ProductService;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+	
+	private final ProductService productService;
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public void createProduct(@RequestBody ProductRequest productRequest) {
+		
+		productService.createProduct(productRequest);
 		
 	}
 
